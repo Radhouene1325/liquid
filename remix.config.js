9b94cdaa8f64bff0b9af5c31244905f876d97cdb
khalfa Radhouene <102431563+Radhouene1325@users.noreply.github.com>
2026-06-14 01:05:54 +0200

    Add Remix configuration

---
+++ remix.config.js
@@ -0,0 +1,25 @@
+import {
+  vitePlugin as remix,
+  flatRoutes,
+} from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+      routes: async (defineRoutes) => {
+        return flatRoutes("routes", defineRoutes);
+      },
+    }),
+    tsconfigPaths(),
+  ],
+  build: {
+    assetsInlineLimit: 0,
+  },
+});
